b1c3ac5571d54701a17faebaf0c8f199d69c8b38
Rahul Chandrashekhar <rahul142chandrashekhar@gmail.com>
2015-10-02 01:25:03 -0400

    Ch 5 Challenge 2
    A bug has been resolved in which once the users get back, they can
    rotate QuizActivity to clear out mIsCheater

--- app/src/main/java/com/example/rahulchandrashekhar/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/rahulchandrashekhar/geoquiz/QuizActivity.java
@@ -17,6 +17,7 @@ public class QuizActivity extends AppCompatActivity {
     private static final String TAG = "QuizActivity";
     private static final String KEY_INDEX = "index";
     private static final int REQUEST_CODE_CHEAT = 0;
+    private static final String KEY_CHEAT_STATE = "isCheater";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -122,6 +123,7 @@ public class QuizActivity extends AppCompatActivity {
 
         if (savedInstanceState != null) {
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+            mIsCheater = savedInstanceState.getBoolean(KEY_CHEAT_STATE, false);
         }
 
         updateQuestion();
@@ -144,6 +146,7 @@ public class QuizActivity extends AppCompatActivity {
         super.onSaveInstanceState(savedInstanceState);
         Log.i(TAG, "onSaveInstanceState");
         savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+        savedInstanceState.putBoolean(KEY_CHEAT_STATE, mIsCheater);
     }
 
     @Override
