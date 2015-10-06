28bc39b00c3d3728c57c5c721c7aadf6057e5af3
Rahul Chandrashekhar <rahul142chandrashekhar@gmail.com>
2015-10-06 09:49:28 -0400

    Added Animation
    Added activity animation code to the quiz

--- app/src/main/java/com/example/rahulchandrashekhar/geoquiz/CheatActivity.java
+++ app/src/main/java/com/example/rahulchandrashekhar/geoquiz/CheatActivity.java
@@ -1,13 +1,17 @@
 package com.example.rahulchandrashekhar.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -63,6 +67,27 @@ public class CheatActivity extends AppCompatActivity {
                 }
 
                 setAnswerShownResult(true);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswer.getWidth() / 2;
+                    int cy = mShowAnswer.getHeight() / 2;
+                    float radius = mShowAnswer.getWidth();
+                    Animator anim = ViewAnimationUtils
+                            .createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mAnswerTextView.setVisibility(View.VISIBLE);
+                            mShowAnswer.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    anim.start();
+                } else {
+                    mAnswerTextView.setVisibility(View.VISIBLE);
+                    mShowAnswer.setVisibility(View.INVISIBLE);
+                }
+
             }
         });
 
